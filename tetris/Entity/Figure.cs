c4d151499dae2a9251e50572b5a8f1157dc5030e
Stas <stas249501@gmail.com>
2014-09-24 14:03:12 +0300

    fix error

--- tetris/Entity/Figure.cs
+++ tetris/Entity/Figure.cs
@@ -210,19 +210,4 @@ namespace tetris
             return elements.GetEnumerator();
         }
     }
-
-
-    class LFigure : Figure
-    {
-        public LFigure(Point p, Heap heap, int sizeItem)
-            : base(heap, sizeItem)
-        {
-            elements = new Point[4];
-            elements[0] = p;
-            elements[1] = new Point(elements[0].X + SizeItem, elements[0].Y);
-            elements[2] = new Point(elements[1].X, elements[1].Y - SizeItem);
-            elements[3] = new Point(elements[2].X, elements[2].Y - SizeItem);
-            central = 2;
-        }
-    }
 }
