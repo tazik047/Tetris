7445fd24b66c61aa231b54b6e9dde2a5d1600eb1
annhv <annhv@yandex.ru>
2014-11-12 21:29:03 +0200

    newBestScore form

---
+++ tetris/NewBestScore.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace tetris
+{
+    partial class NewBestScore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поздравялем! \r\nВы только что установили новый рекорд! ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите Ваше имя:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreText.Location = new System.Drawing.Point(160, 57);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(47, 17);
+            this.scoreText.TabIndex = 3;
+            this.scoreText.Text = "label3";
+            // 
+            // okBtn
+            // 
+            this.okBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.okBtn.Location = new System.Drawing.Point(105, 148);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(92, 32);
+            this.okBtn.TabIndex = 0;
+            this.okBtn.Text = "Запомнить";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelBtn.Location = new System.Drawing.Point(199, 148);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(92, 32);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Пропустить";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // NewBestScore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 195);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NewBestScore";
+            this.Text = "NewBestScore";
+            this.Load += new System.EventHandler(this.NewBestScore_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
---
+++ tetris/NewBestScore.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris
+{
+    public partial class NewBestScore : Form
+    {
+        List<Gamer> gamers = new List<Gamer>();
+        Records recs;
+
+        public NewBestScore()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            recs = new Records();
+            for (int i = 0; i < gamers.Count; i++)
+            {
+                if (gamers[i].Name == "Winner")
+                {
+                    gamers[i].Name = textBox1.Text;
+                    this.Save();
+                    break;
+                }
+            }
+            this.Close();
+            recs.ShowDialog();
+        }
+
+        private void NewBestScore_Load(object sender, EventArgs e)
+        {
+            this.Open();
+            for (int i = 0; i < gamers.Count; i++)
+            {
+                if (gamers[i].Name == "Winner")
+                {
+                    scoreText.Text = "Вас счет: " + gamers[i].Score;
+                    break;
+                }
+            }
+           
+        }
+
+        public void Save()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream("bestScores.dat", FileMode.OpenOrCreate))
+            {
+                binFormat.Serialize(fStream, gamers);
+            }
+        }
+
+        public void Open()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            try
+            {
+                using (Stream fStream = new FileStream("bestScores.dat", FileMode.OpenOrCreate))
+                {
+                    gamers = (List<Gamer>)binFormat.Deserialize(fStream);
+                }
+            }
+            catch { }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < gamers.Count; i++)
+            {
+                if (gamers[i].Name == "Winner")
+                {
+                    gamers[i].Name = "Безымянный";
+                    this.Save();
+                    break;
+                }
+            }
+            this.Close();
+            this.Close();
+        }
+    }
+}
